6799adb83042f939e8bd794fc4106a4a95c95008
Pavel Erikov <prosto.pasha123@gmail.com>
2021-02-09 15:31:03 +0300

    Added add, sub, mul, div, mod functions

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include "calculator.h"
+
+int add_m(int a, int b) {
+    return a + b;
+}
+
+int sub_m(int a, int b) {
+    return a - b;
+}
+
+int mul_m(int a, int b) {
+    return a * b;
+}
+
+int div_m(int a, int b) {
+    return a / b;
+}
+
+int mod_m(int a, int b) {
+    return a % b;
+}
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "calculator.h"
 using namespace std;
 
 int main() {
@@ -12,6 +13,26 @@ int main() {
         cout << "арифметическую операцию (+,-,*,...): ";
         cin >> sign;
         system("cls");
+        switch (sign) {
+        case '+':
+            result = add_m(a, b);
+            break;
+        case '-':
+            result = sub_m(a, b);
+            break;
+        case '*':
+            result = mul_m(a, b);
+            break;
+        case '/':
+            result = div_m(a, b);
+            break;
+        case '%':
+            result = mod_m(a, b);
+            break;
+        default:
+            cout << "Вы ввели не правильную операцию!" << endl;
+            break;
+        }
         cout << "Результат (result =): " << result << endl;
     } while (sign != '0');
     cout << "Итоговый результат: " << result << endl;
